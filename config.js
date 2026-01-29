884a4b34eec2de9caf5a4cc649502fc93dd4ef18
Dennis Mwangi <denochira78@gmail.com>
2026-01-29 17:13:56 +0300

    Connect Google Sheets CSV

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const WHATSAPP_NUMBER = "254705822343";
 
 // Your published Google Sheet CSV URL (Publish to web → CSV).
 // It should look like: https://docs.google.com/spreadsheets/d/e/<id>/pub?gid=0&single=true&output=csv
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR7rJJoHcKR6qEKst-e1k9Jbub-IzhP5I824q69KnuJPdkpXSGwWJFF8t--h3EZIMSV6mYpgyFh6y74/pub?output=csv";
+const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmcdNPVd5mjXYxMa1g-N8uH2vliRTg33iXH0mxMpbTg8obKdIDs9Zs6DdXmMZVCIY1Ni7kMLHLMCgG/pub?output=csv";
 
 // Shop branding
 const SHOP_NAME = "Tabz Collection";
